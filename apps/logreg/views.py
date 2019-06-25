4df1f350298ea20bc41826b97e7ccfbf473ac985
dylanlench <dylanlench@gmail.com>
2019-06-25 16:24:04 -0700

    first commit

---
+++ apps/logreg/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+import bcrypt
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+def main(request):
+    return(ladida)
